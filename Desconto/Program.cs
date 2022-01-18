8290774986ad9f407d0c483282f5e0f33d18f0d5
TI <TI@TI_VISUAL>
2022-01-18 16:45:17 -0300

    Adicionar arquivos de projeto.

---
+++ Desconto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o preço do produto: ");
+
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = 0.0;
+                        
+            if (preco > 100.00)
+            {
+                desconto = preco * 0.1;
+            }
+
+            Console.WriteLine("O valor do desconto é: ");
+            Console.WriteLine(desconto);
+        }
+    }
+}
